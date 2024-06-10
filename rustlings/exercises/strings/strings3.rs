8d064a6400503f82c1930f6d6f80ca72c792a8bf
Adrian-Cespedes <adrian.cespedes@utec.edu.pe>
2024-06-09 22:37:31 -0500

    Some progress

--- rustlings/exercises/strings/strings3.rs
+++ rustlings/exercises/strings/strings3.rs
@@ -3,21 +3,26 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    // let mut result = String::new();
+    // for c in input.chars() {
+    //     if c != ' ' {
+    //         result.push(c);
+    //     }
+    // }
+    // result
+    String::from(input.trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    String::from(input).replace("cars", "balloons")
 }
 
 #[cfg(test)]
@@ -39,7 +44,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
